b5b8c7162cb02cbbf7f2fbf39b0215ebe66d6d10
Dominik Lenda <dlenda1729@gmail.com>
2020-05-26 19:26:37 +0200

    Get files to run one trial of the experiment.

---
+++ script.js
@@ -0,0 +1,8 @@
+// Show an element
+let showAndHide = function (elem) {
+	elem.style.display = 'block';
+  function hide() {
+  return elem.style.display = 'none'
+    }
+  setTimeout(hide, 1000)
+};
